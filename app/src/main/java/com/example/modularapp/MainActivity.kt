f5e9653ca74a3490b3efa16a89bdbb5124841723
Larissa Marães <larissa.maraes@jera.com.br>
2019-06-21 23:50:42 -0400

    main app starting with register activity

--- app/src/main/java/com/example/modularapp/MainActivity.kt
+++ app/src/main/java/com/example/modularapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.modularapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RegisterActivity.startActivity(this)
     }
 }
--- register/src/main/java/com/example/register/RegisterActivity.kt
+++ register/src/main/java/com/example/register/RegisterActivity.kt
@@ -1,7 +1,8 @@
 package com.example.register
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.register.databinding.ActivityRegisterBinding
@@ -10,7 +11,16 @@ class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            context.startActivity(
+                Intent(context, RegisterActivity::class.java)
+            )
+        }
     }
 }
